44840176220f788df0a175f8b27e142cc4d8e082
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-10 12:36:57 +0200

    InterviewTasks

---
+++ CodexioTask/ColorBallsTask/StartUp.cs
@@ -0,0 +1,79 @@
+﻿namespace Task1
+{
+    class Ball
+    {
+        public Ball(string color, int count)
+        {
+            this.Color = color;
+            this.Count = count;
+        }
+
+        public string Color { get; set; }
+        public int Count { get; set; }
+
+    }
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] allowedColors = new string[]
+            {
+                "red",
+                "green",
+                "blue",
+                "yellow",
+                "purple",
+                "black",
+                "white",
+                "orange",
+                "silver",
+                "pink",
+            };
+
+            List<Ball> box = new List<Ball>();
+            int totalBallsCount = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a color or \"End\" to finish the move: ");
+                string color = Console.ReadLine();
+
+                if (color == "End")
+                {
+                    break;
+                }
+
+                if (!allowedColors.Contains(color))
+                {
+                    Console.WriteLine("The color is invalid! Please enter a valid color: ");
+                    continue;
+                }
+
+                Console.WriteLine("Please enter a ball count: ");
+                int ballCount;
+                string ballCountInput = Console.ReadLine();
+                bool success = int.TryParse(ballCountInput, out ballCount);
+
+                if (!success)
+                {
+                    while (!success)
+                    {
+                        Console.WriteLine("Invalid number of balls! Please enter a valid number: ");
+                        ballCountInput = Console.ReadLine();
+                        success = int.TryParse(ballCountInput, out ballCount);
+                    }
+                }
+
+                Ball ball = new Ball(color, ballCount);
+                totalBallsCount += ballCount;
+                box.Add(ball);
+            }
+
+
+            var orderedBalls = box.OrderByDescending(b => b.Count).ToList();
+            var result = totalBallsCount - orderedBalls[0].Count;
+
+            Console.WriteLine($"You have to take out {result} balls, in order to have only one color of balls in the box!");
+        }
+    }
+}
---
+++ CodexioTask/RotationMatrixTask/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace RotationMatrixTask
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int rotationCount = int.Parse(Console.ReadLine());
+
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowInput = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                }
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if(col == matrix.GetLength(1) - 1)
+                    {
+                        Console.Write(matrix[row, col]);
+                        continue;
+                    }
+                    Console.Write(matrix[row, col] + "_");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
