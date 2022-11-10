698d986142344e355ef89b0d773a5a31acbff9fa
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-10 13:52:04 +0200

    Tasks

--- CodexioTask/RotationMatrixTask/StartUp.cs
+++ CodexioTask/RotationMatrixTask/StartUp.cs
@@ -6,9 +6,10 @@
         {
             int rotationCount = int.Parse(Console.ReadLine());
 
-            int matrixSize = int.Parse(Console.ReadLine());
+            int matrixRow = int.Parse(Console.ReadLine());
+            int matrixCol = int.Parse(Console.ReadLine());
 
-            int[,] matrix = new int[matrixSize, matrixSize];
+            int[,] matrix = new int[matrixRow, matrixCol];
 
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
@@ -19,9 +20,32 @@
                 }
             }
 
+            for (int i = 0; i < rotationCount; i++)
+            {
+                matrix = RotateMatrix(matrix);
+            }
+
             PrintMatrix(matrix);
         }
 
+        public static int[,] RotateMatrix(int[,] currentMatrix)
+        {
+            var x = currentMatrix.GetLength(1);
+            var y = currentMatrix.GetLength(0);
+            var newMatrix = new int[x, y];
+            var newRow = 0;
+
+            for (var oldColumn = x - 1; oldColumn >= 0; oldColumn--)
+            {
+                for (var oldRow = 0; oldRow < y; oldRow++)
+                {
+                    newMatrix[newRow, oldRow] = currentMatrix[oldRow, oldColumn];
+                }
+
+                newRow++;
+            }
+            return newMatrix;
+        }
         public static void PrintMatrix(int[,] matrix)
         {
             for (int row = 0; row < matrix.GetLength(0); row++)
@@ -39,5 +63,7 @@
                 Console.WriteLine();
             }
         }
+
+       
     }
 }
